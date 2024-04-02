1695918822ce1ca8c879d027955291a879c5c76b
Diego Santana <3c.santana@gmail.com>
2024-04-02 09:04:02 -0300

    feat: implementação do programa principal

--- desafio1/src/main/java/com/devsuperior/desafio1/Desafio1Application.java
+++ desafio1/src/main/java/com/devsuperior/desafio1/Desafio1Application.java
@@ -1,13 +1,35 @@
 package com.devsuperior.desafio1;
 
+import com.devsuperior.desafio1.entities.Order;
+import com.devsuperior.desafio1.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Desafio1Application {
+public class Desafio1Application implements CommandLineRunner {
+	
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Order order1 = new Order(1034, 150.0, 20.0);
+		Order order2 = new Order(2282, 800.0, 10.0);
+		Order order3 = new Order(1309, 95.90, 0.0);
 
+		System.out.println("Pedido código " +  order1.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order1));
+		
+		System.out.println("Pedido código " +  order2.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order2));
+		
+		System.out.println("Pedido código " +  order3.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order3));
+	}
 }
