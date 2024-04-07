5ea2157fbb391f8e86dd94d30ab9496d6e93e292
Diego Santana <3c.santana@gmail.com>
2024-04-07 16:15:07 -0300

    feat: Definindo os atributos de data como TIMESTAMP sem time zone

--- sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Block.java
+++ sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Block.java
@@ -12,7 +12,9 @@ public class Block {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant start;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant finish;
     
     @ManyToOne
