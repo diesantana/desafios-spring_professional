f9b3e6d208df2d08d93ecc6ed4fc3a5d97198a6e
Diego Santana <3c.santana@gmail.com>
2024-04-07 13:55:41 -0300

    feat: Criação da entidade Participant

---
+++ sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Participant.java
@@ -0,0 +1,9 @@
+package com.devsuperior.sistemaevento.entities;
+
+public class Participant {
+    
+    private Integer id;
+    private String name;
+    private String email;
+    // atividades
+}
