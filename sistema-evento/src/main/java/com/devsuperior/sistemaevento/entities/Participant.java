3d9a1e180a6a64091461eaafb7a6330d2aed5423
Diego Santana <3c.santana@gmail.com>
2024-04-07 16:34:39 -0300

    feat: Criação do atributo participants e mapamento muitos-para-muitos

--- sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Participant.java
+++ sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Participant.java
@@ -1,9 +1,73 @@
 package com.devsuperior.sistemaevento.entities;
 
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "tb_participant")
 public class Participant {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    @Column(unique = true)
     private String email;
-    // atividades
+    
+    @ManyToMany
+    @JoinTable(name = "tb_participant_activity",
+            joinColumns = @JoinColumn(name = "participant_id"),
+            inverseJoinColumns = @JoinColumn(name = "activity_id"))
+    private Set<Activity> activities = new HashSet<>();
+
+    public Participant() {
+    }
+
+    public Participant(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Activity> getActivities() {
+        return activities;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participant that)) return false;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
