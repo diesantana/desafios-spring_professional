27f20fd0b9bc93d8d0b897a93c79f0cf1e61b37f
Diego Santana <3c.santana@gmail.com>
2024-04-07 16:08:44 -0300

    feat: Criação do atributo activity e mapeamento muitos-para-um

--- sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Block.java
+++ sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Block.java
@@ -1,10 +1,76 @@
 package com.devsuperior.sistemaevento.entities;
 
-import java.time.Instant;
+import jakarta.persistence.*;
 
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_block")
 public class Block {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Instant start;
     private Instant finish;
+    
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+    public Block() {
+    }
+
+    public Block(Integer id, Activity activity, Instant finish, Instant start) {
+        this.id = id;
+        this.activity = activity;
+        this.finish = finish;
+        this.start = start;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getStart() {
+        return start;
+    }
+
+    public void setStart(Instant start) {
+        this.start = start;
+    }
+
+    public Instant getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Instant finish) {
+        this.finish = finish;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Block block)) return false;
+
+        return Objects.equals(id, block.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
