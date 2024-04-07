daca43df8382b5a8510d2ec656679d35263dd10b
Diego Santana <3c.santana@gmail.com>
2024-04-07 13:55:50 -0300

    feat: Criação da entidade Block

---
+++ sistema-evento/src/main/java/com/devsuperior/sistemaevento/entities/Block.java
@@ -0,0 +1,10 @@
+package com.devsuperior.sistemaevento.entities;
+
+import java.time.Instant;
+
+public class Block {
+    
+    private Integer id;
+    private Instant start;
+    private Instant finish;
+}
